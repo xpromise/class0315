f7a774321d8ab71188a40b61d9cea3412f76852f
xiongjian <webjsforyou@gmail.com>
2018-06-25 17:24:42 +0800

    跨域和解决方案

--- day05/02.jQueryAJAX.js
+++ day05/02.jQueryAJAX.js
@@ -5,7 +5,23 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/ajax', (req, res) => {
-  res.send('服务器返回的响应');
+  //接受jsonp发送过来的请求参数
+  const {callback} = req.query;
+  const data = {
+    username: '孙悟空'
+  };
+  //将其拼接成函数调用的方式，最终将拼接成的东西转化为json字符串
+  // const data = JSON.stringify(data);
+  res.send(callback + '(' +  JSON.stringify(data) + ')');  //fun({"username": "孙悟空"})
+  // res.send("window.data = '服务器返回的响应'");
+})
+
+app.get('/cors', (req, res) => {
+  //允许所有网址都可以跨域
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  //允许指定的网址进行跨域
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
+  res.send('cors返回的响应');
 })
 
 app.post('/ajax', (req, res) => {
