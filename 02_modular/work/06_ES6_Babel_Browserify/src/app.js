bf3488f7ee344ff2c621e2470391e31d86244a0f
xiongjian <webjsforyou@gmail.com>
2018-07-03 15:51:02 +0800

    ES6的模块化语法扩展

--- 02_modular/work/06_ES6_Babel_Browserify/src/app.js
+++ 02_modular/work/06_ES6_Babel_Browserify/src/app.js
@@ -1,14 +1,18 @@
 /*
   引入模块
     import {模块中暴露内容} from '模块路径'
+      动态加载，没有缓存
  */
 
-import {foo1, name} from './module1';  //引入分别暴露
+// import {foo1, name} from './module1';  //引入分别暴露
+import * as m1 from './module1';
 import {fn1, fn2} from './module2';    //引入统一暴露
 import Person from './module3'         //引入默认暴露
 
-foo1();
-console.log(name);
+// foo1();
+// console.log(name);
+m1.foo1();
+console.log(m1.name);
 fn1();
 fn2();
 const p = new Person('Jack');
--- 02_modular/work/06_ES6_Babel_Browserify/src/module3.js
+++ 02_modular/work/06_ES6_Babel_Browserify/src/module3.js
@@ -1,5 +1,6 @@
 /*
-  默认暴露
+  默认暴露：只能暴露一个内容
+    默认暴露的本质：定义了default变量，将后面的值赋值给default变量，暴露出去
  */
 
 function Person(name) {
