8d0061f59a417979af4cfb0d5fd8fb1cb7002893
xiongjian <webjsforyou@gmail.com>
2018-07-03 10:11:22 +0800

    模块化

--- day01/06_npm.js
+++
@@ -1,4 +0,0 @@
-const math = require('math');
-
-console.log(math.add(2, 3));
-console.log(math.mul(2, 3));
--- day01/myModule02.js
+++
@@ -1,7 +0,0 @@
-//定义模块的内容
-const boy = 'jack';
-const girl = 'rose';
-
-//暴露模块内容
-exports.boy = boy;
-exports.girl = girl;
