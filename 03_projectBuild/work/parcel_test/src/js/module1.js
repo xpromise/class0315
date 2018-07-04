65b5307ad5f3ae58ef4c7df5974bfcbde3db161f
xiongjian <webjsforyou@gmail.com>
2018-07-04 17:02:06 +0800

    parcel

---
+++ 03_projectBuild/work/parcel_test/src/js/module1.js
@@ -0,0 +1,12 @@
+(() => {
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(10, 20));
+  console.log(add(20, 20));
+  console.log(add(30, 20));
+  console.log(add(40, 20));
+  console.log(add(50, 20));
+  console.log(add(60, 20));
+  console.log(add(70, 20));
+})();
