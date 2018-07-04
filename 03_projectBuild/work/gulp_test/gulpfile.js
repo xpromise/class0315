3c529c7fd785a2c7e36ac7c1b212eea9e26f4056
xiongjian <webjsforyou@gmail.com>
2018-07-04 16:29:42 +0800

    gulp完整版

--- 03_projectBuild/work/gulp_test/gulpfile.js
+++ 03_projectBuild/work/gulp_test/gulpfile.js
@@ -1,15 +1,20 @@
 //引入gulp模块
 const gulp = require('gulp');
-const jshint = require('gulp-jshint');
+const $ = require('gulp-load-plugins')();
+/*const jshint = require('gulp-jshint');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
-const less = require('gulp-less');
+const less = require('gulp-less');*/
 const LessAutoprefix = require('less-plugin-autoprefix');
 const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
-const cssmin = require('gulp-cssmin');
+/*const cssmin = require('gulp-cssmin');
 const htmlmin = require('gulp-htmlmin');
+const livereload = require('gulp-livereload');
+const connect = require('gulp-connect');*/
+const open = require('open');
+const opn = require('opn');
 /*
   任务如果指定了 return  那么这个任务就是个异步的任务
       如果没有指定 return  那么这个任务就是个同步的任务
@@ -45,39 +50,58 @@ gulp.task('uglify', ['concat'], function () {
 // 检查语法错误 -- 进行语法转换 -- 合并js文件 -- 压缩js文件
 gulp.task('minifyjs', function () {
   return gulp.src('./src/js/*.js')
-    .pipe(jshint({esversion: 6}))    //检查js语法错误
-    .pipe(jshint.reporter('default'))  //使用默认的错误提示
-    .pipe(babel({       //语法转换 es6-es5
+    .pipe($.jshint({esversion: 6}))    //检查js语法错误
+    .pipe($.jshint.reporter('default'))  //使用默认的错误提示
+    .pipe($.babel({       //语法转换 es6-es5
         presets: ['es2015']
     }))
     .pipe(gulp.dest('build/js'))
-    .pipe(concat('built.js'))   //合并所有js文件并命名
+    .pipe($.concat('built.js'))   //合并所有js文件并命名
     .pipe(gulp.dest('./build/js'))
-    .pipe(uglify())     //压缩js代码
-    .pipe(rename('dist.min.js'))   //重命名js文件
+    .pipe($.uglify())     //压缩js代码
+    .pipe($.rename('dist.min.js'))   //重命名js文件
     .pipe(gulp.dest('dist/js'))
+    .pipe($.livereload())
 })
 
 gulp.task('minifycss', function () {
   return gulp.src('src/less/*.less')
-    .pipe(less({          //将less文件编译成css文件
+    .pipe($.less({          //将less文件编译成css文件
       plugins: [autoprefix]  //增加前缀
     }))
     .pipe(gulp.dest('build/css'))
-    .pipe(concat('built.css'))   //合并css文件
+    .pipe($.concat('built.css'))   //合并css文件
     .pipe(gulp.dest('build/css'))
-    .pipe(cssmin())   //压缩css
-    .pipe(rename('dist.min.css'))
+    .pipe($.cssmin())   //压缩css
+    .pipe($.rename('dist.min.css'))
     .pipe(gulp.dest('./dist/css'))
+    .pipe($.livereload())
 })
 
 gulp.task('minifyhtml', function () {
   return gulp.src('src/index.html')
-    .pipe(htmlmin({
+    .pipe($.htmlmin({
       removeComments: true,
       collapseWhitespace: true
     }))
     .pipe(gulp.dest('dist'))
+    .pipe($.livereload())
+})
+
+gulp.task('watch', ['default'], function () {
+  $.livereload.listen();
+  //配置热更新/热加载
+  $.connect.server({
+    root: 'dist',   //访问目录
+    livereload: true,
+    port: 3000
+  });
+  //打开网页
+  opn('http://localhost:3000');
+  //配置监视任务
+  gulp.watch('./src/js/*.js', ['minifyjs'])
+  gulp.watch('./src/less/*.less', ['minifycss'])
+  gulp.watch('./src/index.html', ['minifyhtml'])
 })
 
 gulp.task('default', ['minifyjs', 'minifycss', 'minifyhtml'])//异步执行
--- 03_projectBuild/work/gulp_test/src/js/module1.js
+++ 03_projectBuild/work/gulp_test/src/js/module1.js
@@ -4,4 +4,7 @@
   }
   console.log(add(10, 20));
   console.log(add(20, 20));
+  console.log(add(30, 20));
+  console.log(add(40, 20));
+  console.log(add(50, 20));
 })();
