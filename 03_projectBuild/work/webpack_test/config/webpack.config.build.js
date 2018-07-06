008ca36e86d02989caf1b8e48b32e289da976c1f
xiongjian <webjsforyou@gmail.com>
2018-07-06 11:55:25 +0800

    build环境的搭建

--- 03_projectBuild/work/webpack_test/config/webpack.config.build.js
+++ 03_projectBuild/work/webpack_test/config/webpack.config.build.js
@@ -6,13 +6,16 @@
 const {resolve} = require('path');
 //提取css成单独的文件
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+//创建一个html文件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//清除指定目录
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   //入口
   entry: './src/js/app.js',
   //输出
   output: {
-    path: resolve(__dirname, 'build'),   //输出目录
+    path: resolve(__dirname, '../build'),   //输出目录
     filename: './js/built.js'            //输出文件名
   },
   //配置loader
@@ -66,5 +69,12 @@ module.exports = {
   //配置插件
   plugins: [
     new ExtractTextPlugin("./css/built.css"),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',     //文件名
+      template: 'src/index.html'  //以指定html文件为模板去创建html文件
+    }),
+    new CleanWebpackPlugin('../build', {  //不遵循output输出目录
+      allowExternal: true   //允许清除根目录以外的文件夹
+    })
   ]
 }
