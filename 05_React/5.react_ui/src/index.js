a69b44794f05f6ac0b7f918f7f208dd962d65a5b
xiongjian <webjsforyou@gmail.com>
2018-07-13 10:42:50 +0800

    按需加载

--- 05_React/5.react_ui/src/index.js
+++ 05_React/5.react_ui/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'antd-mobile/dist/antd-mobile.css';
-
+// import 'antd-mobile/dist/antd-mobile.css';
 import App from './components/app/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
